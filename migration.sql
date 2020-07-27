7f51df8c79a2c0489e76f77aa2cf8fc77dd4180d
Armando Roman <armando.roman.jr20@gmail.com>
2020-07-27 11:40:32 -0500

    commiting

--- migration.sql
+++ migration.sql
@@ -2,6 +2,7 @@ USE adlister_db;
 
 DROP TABLE IF EXISTS ads;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS categories;
 
 CREATE TABLE users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -18,5 +19,13 @@ CREATE TABLE ads (
     description TEXT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE categories (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    category VARCHAR(240) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES ads(id)
         ON DELETE CASCADE
 );
