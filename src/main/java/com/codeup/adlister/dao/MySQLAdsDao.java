b88fb64d89b7806cb7a7b1dd268ee570f6ee60e1
Julia Contreras <juliacontreras483@gmail.com>
2020-07-29 23:46:46 -0500

    view ads on profile

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -26,7 +26,7 @@ public class MySQLAdsDao implements Ads {
     public List<Ad> adsByUser(long id) {
         PreparedStatement stmt = null;
         try {
-            String insertQuery = "SELECT * FROM ads WHERE id = ?";
+            String insertQuery = "SELECT * FROM ads WHERE  user_id = ?";
             stmt = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
             stmt.setLong(1, id);
             ResultSet rs = stmt.executeQuery();
