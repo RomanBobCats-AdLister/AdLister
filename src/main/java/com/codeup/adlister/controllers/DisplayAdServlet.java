e6c91439bbeda7c57aba6927f631d0492241c3fd
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-29 22:50:12 -0500

    fixed something that you guys need

--- src/main/java/com/codeup/adlister/controllers/DisplayAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DisplayAdServlet.java
@@ -19,19 +19,14 @@ public class DisplayAdServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-<<<<<<< HEAD
-        User user = (User) req.getSession().setAttribute("user");
-        long adId = Long.parseLong(req.getParameter("id"));
-        Ad ad = DaoFactory.getAdsDao().findById(adId);
-        req.setAttribute("ad", ad);
 
 
-=======
+
         User user = (User) req.getSession().getAttribute("user");
         String adId = req.getPathInfo().substring(1);
         Ad ad = DaoFactory.getAdsDao().findById(parseInt((adId)));
->>>>>>> master
 
+        req.setAttribute("ad", ad);
         req.setAttribute("user", user);
 
         req.getRequestDispatcher("/WEB-INF/ads/displayAd.jsp").forward(req, resp);
